c9827ee0c77fd110ed6b69927277986c077b17cf
Gabriel Vilches <vilchesalves@gmail.com>
2020-06-01 20:31:33 +1200

    implement horse data in the form

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,58 @@ import './App.css';
 import useHorses from './services/useHorses';
 
 function App() {
-  const { data, loading, errors } = useHorses();
+  const { data: horses, loading, errors } = useHorses();
+  const [selectedHorseId, setSelectedHorseId] = React.useState('');
+  const selectedHorse = horses?.find((horse) => horse.id === selectedHorseId);
+
+  const selectHorse = (id) => {
+    setSelectedHorseId(id);
+  };
 
   return (
     <div className="App">
-      {loading && (
-        <img src={logo} className="App-logo" alt="logo" />
-      )}
-      {data && (
-        <ul>
-          {data.slice(0, 10).map((horse) => (
-            <li key={horse.id}>{horse.name}</li>
-          ))}
-        </ul>
-      )}
-      {errors && (
-        <p>Horses could not be loaded.</p>
+      <header>
+        {loading && (
+          <img src={logo} className="App-logo" alt="logo" />
+        )}
+        {horses && (
+          <ul>
+            {horses.slice(0, 10).map((horse) => (
+              <li key={horse.id}>
+                <button
+                  type="button"
+                  onClick={() => { selectHorse(horse.id); }}
+                >
+                  {horse.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+        {errors && (
+          <p>Horses could not be loaded.</p>
+        )}
+      </header>
+      {selectedHorse && (
+        <section>
+          <h1>Details</h1>
+          <label htmlFor="name">
+            Name
+            <input id="name" value={selectedHorse.name} />
+          </label>
+          <label htmlFor="favouriteFood">
+            favouriteFood
+            <input id="favouriteFood" value={selectedHorse.profile.favouriteFood} />
+          </label>
+          <label htmlFor="weight">
+            weight
+            <input id="weight" value={selectedHorse.profile.physical.weight} />
+          </label>
+          <label htmlFor="height">
+            height
+            <input id="height" value={selectedHorse.profile.physical.height} />
+          </label>
+        </section>
       )}
     </div>
   );
